f313cfee81a959d8371db758a5f531825b67a34d
DuppahMeamnim <DuppahMeamnim@gmail.com>
2024-11-06 21:54:13 +0200

    add info

--- DMGuesser/src/DamageData/damageData.tsx
+++ DMGuesser/src/DamageData/damageData.tsx
@@ -1,8 +1,7 @@
 export default interface damageData {
     image: string,
     word: string,
-    distance: string,
     weapon: string,
     hitLocation: string,
-    wentThrough: string
+    wentThrough: string[]
 } 
