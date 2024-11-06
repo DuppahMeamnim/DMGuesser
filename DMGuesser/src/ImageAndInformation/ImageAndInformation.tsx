fdf4b754a44456a18c84531d3365e73d2003955f
DuppahMeamnim <DuppahMeamnim@gmail.com>
2024-11-06 22:00:10 +0200

    add animation

--- DMGuesser/src/ImageAndInformation/ImageAndInformation.tsx
+++ DMGuesser/src/ImageAndInformation/ImageAndInformation.tsx
@@ -22,23 +22,28 @@ export default function ImageAndInformation(damageData: damageData) {
                 height="100%"
                 fit="fill"
                 src={damageData.image}
+                style={{
+                    borderRadius: isMobile ? "4vw" : "1vw"
+                }}
             />
             {
-                opened &&
                 <Flex
                     onClick={() => { opened ? close() : open() }}
                     w={isMobile ? "80vw" : "28vw"}
                     h={isMobile ? "43vw" : "15vw"}
                     style={{
+                        borderRadius: isMobile ? "4vw" : "1vw",
                         position: 'absolute',
                         top: '50%',
                         left: '50%',
-                        transform: 'translate(-50%, -50%)',
                         backgroundColor: 'rgba(0, 0, 0, 0.5)',
                         color: 'white',
                         flexDirection: "column",
                         justifyContent: "space-evenly",
                         alignItems: "center",
+                        opacity: opened ? 1 : 0,
+                        transform: opened ? 'translate(-50%, -50%) scale(1)' : 'translate(-50%, -50%) scale(0.9)',
+                        transition: 'opacity 0.3s ease, transform 0.3s ease', // smooth fade and scale transitions
                     }}>
                     <Text ml=".75vw" size={isMobile ? "4vw" : "1.5vw"}>Hit at: {damageData.hitLocation}</Text>
                     <Text ml=".75vw" size={isMobile ? "4vw" : "1.5vw"}>Weapon: {damageData.weapon}</Text>
