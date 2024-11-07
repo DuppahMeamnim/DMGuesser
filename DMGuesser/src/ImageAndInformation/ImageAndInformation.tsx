58b79cee2f4a9c488e6fdb4b7398827d25a040fd
= <=>
2024-11-07 09:58:33 +0200

    added image info indicator

--- DMGuesser/src/ImageAndInformation/ImageAndInformation.tsx
+++ DMGuesser/src/ImageAndInformation/ImageAndInformation.tsx
@@ -1,6 +1,7 @@
 import { useDisclosure, useMediaQuery } from "@mantine/hooks";
 import damageData from "../DamageData/damageData";
-import { Flex, Image, Paper, Text } from "@mantine/core";
+import { ActionIcon, Flex, Image, Paper, Text } from "@mantine/core";
+import { Info } from "react-feather";
 
 export default function ImageAndInformation(damageData: damageData) {
     const [opened, { close, open }] = useDisclosure(false);
@@ -15,8 +16,24 @@ export default function ImageAndInformation(damageData: damageData) {
             bg="rgba(0, 0, 0, 0)"
             style={{ position: 'relative', display: 'inline-block' }}
         >
+            <ActionIcon
+                variant="transparent"
+                onClick={() => { opened ? close() : open() }}
+                style={{
+                    position: 'absolute',
+                    top: '3vw',
+                    right: '3vw',                    
+                    color: 'white',
+                    borderRadius: '50%',
+                    opacity: opened ? 0 : 1,
+                    transform: opened ? 'scale(0.9)' : 'scale(1)',
+                    transition: 'opacity 0.3s ease, transform 0.3s ease',
+                }}
+            >
+                <Info size={isMobile ? "4vw" : "2vw"} />
+            </ActionIcon>
+
             <Image
-                // eslint-disable-next-line @typescript-eslint/no-unused-expressions
                 onClick={() => { opened ? close() : open() }}
                 radius={15}
                 width="100%"
@@ -27,9 +44,9 @@ export default function ImageAndInformation(damageData: damageData) {
                     borderRadius: isMobile ? "4vw" : "1vw"
                 }}
             />
+
             {
                 <Flex
-                    // eslint-disable-next-line @typescript-eslint/no-unused-expressions
                     onClick={() => { opened ? close() : open() }}
                     w={isMobile ? "80vw" : "28vw"}
                     h={isMobile ? "43vw" : "15vw"}
@@ -46,12 +63,13 @@ export default function ImageAndInformation(damageData: damageData) {
                         opacity: opened ? 1 : 0,
                         transform: opened ? 'translate(-50%, -50%) scale(1)' : 'translate(-50%, -50%) scale(0.9)',
                         transition: 'opacity 0.3s ease, transform 0.3s ease', // smooth fade and scale transitions
-                    }}>
+                    }}
+                >
                     <Text ml=".75vw" size={isMobile ? "4vw" : "1.5vw"}>Hit at: {damageData.hitLocation}</Text>
                     <Text ml=".75vw" size={isMobile ? "4vw" : "1.5vw"}>Weapon: {damageData.weapon}</Text>
                     <Text ml=".75vw" size={isMobile ? "4vw" : "1.5vw"}>Went through: {damageData.wentThrough.join(", ")}</Text>
                 </Flex>
             }
         </Paper>
-    )
+    );
 }
